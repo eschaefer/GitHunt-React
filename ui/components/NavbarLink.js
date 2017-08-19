4e0eacd62a4a2b188e7152490e0d460d841f1f70
Eric Schaefer <eric.schaefer@native-instruments.de>
2017-08-19 20:27:07 +0200

    Change Link to come from react-router-dom

--- ui/components/NavbarLink.js
+++ ui/components/NavbarLink.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router';
+import { Link } from 'react-router-dom';
 
 const NavbarLink = ({ title, href, active = false }) => (
   <li className={active && 'active'}>
